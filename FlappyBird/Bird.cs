e0c9bc0e1a8217f809c87ebb74ea4d4ff709aec6
arongeo <58132897+arongeo@users.noreply.github.com>
2024-04-05 00:58:16 +0200

    Initial upload

---
+++ FlappyBird/Bird.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlappyBird
+{
+    class Bird
+    {
+        private const int JUMP_SIZE = 3;
+        private const double FALL_PER_TICK = 0.4;
+        private const double JUMP_PER_TICK = 1.0;
+
+        // Az X es Y poziciok invertalva vannak, mert a Console origoja az a bal felso sarok.
+        private int _xPos;
+        private double _yPos;
+        private int _jumpsLeft;
+
+        public int YPos
+        {
+            get
+            {
+                return (int)(Math.Round(_yPos));
+            }
+        }
+
+        public int XPos
+        {
+            get { return _xPos; }
+        }
+
+        public byte BirdGFX
+        {
+            get
+            {
+                return (byte)'@';
+            }
+        }
+
+        public Bird(int startXPos, double startYPos)
+        {
+            this._xPos = startXPos;
+            this._yPos = startYPos;
+            this._jumpsLeft = 0;
+        }
+
+        public void Jump()
+        {
+            this._jumpsLeft += JUMP_SIZE;
+        }
+
+        public void Tick()
+        {
+            if (0 < _jumpsLeft)
+            {
+                this._yPos -= JUMP_PER_TICK;
+                this._jumpsLeft--;
+            } else
+            {
+                this._yPos += FALL_PER_TICK;
+            }
+        }
+    }
+}
---
+++ FlappyBird/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Xml.Serialization;
+using System.CodeDom;
+
+namespace FlappyBird
+{
+    struct Jump
+    {
+        public int startPositionX;
+        public int startPositionY;
+        public double currPositionX;
+        public double currPositionY;
+        public int jumpLeft;
+
+        public Jump(int startPositionX, int startPositionY, int jumpLeft)
+        {
+            this.startPositionX = startPositionX;
+            this.startPositionY = startPositionY;
+            this.currPositionX = 0;
+            this.currPositionY = 0;
+            this.jumpLeft = jumpLeft;
+        }
+    }
+
+    class Program
+    {
+        private const int FRAME_RATE = 30;
+
+        static void Main(string[] args)
+        {
+            byte[] frameBuf = new byte[Console.WindowWidth * (Console.WindowHeight - 1)];
+
+            Console.CursorVisible = false;
+            var stdOut = Console.OpenStandardOutput(Console.WindowWidth * (Console.WindowHeight - 1));
+            
+            bool running = true;
+
+            Bird bird = new Bird(Console.WindowWidth / 2, Console.WindowHeight / 2);
+            frameBuf[(bird.YPos * Console.WindowWidth) + bird.XPos] = bird.BirdGFX;
+            stdOut.Write(frameBuf, 0, Console.WindowWidth * (Console.WindowHeight - 1));
+
+            Console.ReadKey();
+
+            long now = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+
+            while (running)
+            {
+                Thread.Sleep((1000/FRAME_RATE) - (int)((DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond) - now));
+                stdOut.Write(frameBuf, 0, Console.WindowWidth * (Console.WindowHeight - 1));
+                now = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+
+                if (Console.KeyAvailable)
+                {
+                    var consoleKey = Console.ReadKey();
+
+                    if (consoleKey.Key == ConsoleKey.Escape)
+                    {
+                        running = false;
+                        break;
+                    } else if (consoleKey.Key == ConsoleKey.Spacebar)
+                    {
+                        bird.Jump();
+                    }
+                }
+
+                if (0 < bird.YPos)
+                    frameBuf[(bird.YPos * Console.WindowWidth) + bird.XPos] = (byte)' ';
+                
+                bird.Tick();
+
+                if (0 < bird.YPos)
+                    frameBuf[(bird.YPos * Console.WindowWidth) + bird.XPos] = bird.BirdGFX;
+
+                Console.SetCursorPosition(0, 0);
+            }
+
+            Console.Clear();
+            Console.WriteLine("GameOver");
+
+            Console.ReadKey();
+        }
+    }
+}
